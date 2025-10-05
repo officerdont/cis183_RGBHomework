36f4dc0302c501b18f17c042a8d5eeb415a06e14
officerdont <Athorn1101@gmail.com>
2025-10-05 15:15:05 -0400

    finished
    yep

--- app/src/main/java/com/example/cis183_rgbhomework/ColorDataAdapter.java
+++ app/src/main/java/com/example/cis183_rgbhomework/ColorDataAdapter.java
@@ -1,23 +1,42 @@
 package com.example.cis183_rgbhomework;
 
+import static android.graphics.Color.parseColor;
+import static android.graphics.Color.rgb;
+
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.util.ArrayList;
 
 public class ColorDataAdapter extends BaseAdapter
 {
+    public interface OnColorCellClickListener
+    {
+        void onColorCellClick(ColorData color);
+    }
     Context context;
     ArrayList<ColorData> listofcolors;
+    private OnColorCellClickListener clicklistener;
+
 
     public ColorDataAdapter(Context c, ArrayList<ColorData> ls)
     {
       context = c;
       listofcolors = ls;
+        if (c instanceof OnColorCellClickListener) {
+            this.clicklistener = (OnColorCellClickListener) c;
+        } else {
+            throw new RuntimeException(c.toString() + " must implement OnColorCellClickListener");
+        }
+
     }
     @Override
     public int getCount() {
@@ -41,19 +60,59 @@ public class ColorDataAdapter extends BaseAdapter
        if(view == null)
        {
            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(MainActivity.LAYOUT_INFLATER_SERVICE);
+           view = mInflater.inflate(R.layout.color_cell, null);
 
 
        }
         TextView red = view.findViewById(R.id.tv_v_cell_red);
+       TextView red2 = view.findViewById(R.id.tv_red);
         TextView green = view.findViewById(R.id.tv_v_cell_green);
+        TextView green2 = view.findViewById(R.id.tv_green);
         TextView blue = view.findViewById(R.id.tv_v_cell_blue);
+        TextView blue2 = view.findViewById(R.id.tv_blue);
         TextView hexa = view.findViewById(R.id.tv_v_cell_hexa);
+        TextView hexa2 = view.findViewById(R.id.tv_hexa);
+        FrameLayout fl = view.findViewById(R.id.fl_v_cell_container);
+        ConstraintLayout Main2 = view.findViewById(R.id.main);
 
         ColorData color = listofcolors.get(position);
         red.setText(Integer.toString(color.getRed()));
         blue.setText(Integer.toString(color.getBlue()));
         green.setText(Integer.toString(color.getGreen()));
+        int backgroundColor = rgb(color.getRed(),color.getGreen(),color.getBlue());
+        fl.setBackgroundColor(rgb(color.getRed(),color.getGreen(),color.getBlue()));
         hexa.setText(color.getHexadecimal());
+
+        if (androidx.core.graphics.ColorUtils.calculateLuminance(backgroundColor) < 0.5) {
+            // Background is dark, set text to white
+            red.setTextColor(Color.WHITE);
+            green.setTextColor(Color.WHITE);
+            blue.setTextColor(Color.WHITE);
+            hexa.setTextColor(Color.WHITE);
+            red2.setTextColor(Color.WHITE);
+            green2.setTextColor(Color.WHITE);
+            blue2.setTextColor(Color.WHITE);
+            hexa2.setTextColor(Color.WHITE);
+
+        } else {
+            // Background is light, set text to black (or your default color)
+            red.setTextColor(Color.BLACK);
+            green.setTextColor(Color.BLACK);
+            blue.setTextColor(Color.BLACK);
+            hexa.setTextColor(Color.BLACK);
+            red2.setTextColor(Color.BLACK);
+            green2.setTextColor(Color.BLACK);
+            blue2.setTextColor(Color.BLACK);
+            hexa2.setTextColor(Color.BLACK);
+        }
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicklistener.onColorCellClick(color);
+            }
+        });
+
+
 
 
         return view;
--- app/src/main/java/com/example/cis183_rgbhomework/MainActivity.java
+++ app/src/main/java/com/example/cis183_rgbhomework/MainActivity.java
@@ -1,22 +1,47 @@
 package com.example.cis183_rgbhomework;
 
+import static android.graphics.Color.rgb;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.slider.RangeSlider;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ColorDataAdapter.OnColorCellClickListener {
 
     ListView lv_j_ListOfColors;
     private ArrayList<ColorData> ListOfColors;
-    ColorDataAdapter clAdapter;
+    private ColorDataAdapter clAdapter;
+    TextView tv_j_red;
+    TextView tv_j_green;
+    TextView tv_j_blue;
+    SeekBar sb_j_red;
+    SeekBar sb_j_green;
+    SeekBar sb_j_blue;
+    Button btn_j_addcolor;
+    TextView tv_j_hexa;
+    ConstraintLayout main;
+
+    TextView tv_j_hexatext;
+    TextView tv_j_red3;
+    TextView tv_j_green2;
+    TextView tv_j_blue2;
+
+   // ConstraintLayout Main2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,22 +53,180 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         lv_j_ListOfColors = findViewById(R.id.lv_v_listofcolors);
-        ColorData color = new ColorData();
-        ArrayList<ColorData> ListOfColors = new ArrayList<>();
+        tv_j_red = findViewById(R.id.tv_v_red);
+        tv_j_green = findViewById(R.id.tv_v_green);
+        tv_j_blue = findViewById(R.id.tv_v_blue);
+        sb_j_red = findViewById(R.id.sb_v_red);
+        sb_j_green = findViewById(R.id.sb_v_green);
+        sb_j_blue = findViewById(R.id.sb_v_blue);
+        btn_j_addcolor = findViewById(R.id.btn_v_addcolor);
+        main = findViewById(R.id.main);
+        tv_j_hexa = findViewById(R.id.tv_v_hexa);
+        tv_j_hexatext = findViewById(R.id.tv_v_hexatext);
+        tv_j_red3 = findViewById(R.id.tv_v_red3);
+        tv_j_green2 = findViewById(R.id.tv_v_green2);
+        tv_j_blue2 = findViewById(R.id.tv_v_blue2);
+
+       // Main2 = findViewById(R.id.Main2);
+
+
+
         ListOfColors = new ArrayList<>();
+        ColorData color = new ColorData();
+        clAdapter = new ColorDataAdapter(this, ListOfColors);
+        lv_j_ListOfColors.setAdapter(clAdapter);
+
+
 
        color.setRed(7);
        color.setGreen(7);
        color.setBlue(7);
        color.setHexadecimal("7777");
        ListOfColors.add(color);
-     filllistview();
+
+       filllistview();
+       onseekbarchangelistner();
+       onButtonClickListner();
 
 
     }
+    public void onColorCellClick(ColorData color)
+    {
+        sb_j_red.setProgress(color.getRed());
+        tv_j_red.setText(Integer.toString(color.getRed()));
+        sb_j_green.setProgress(color.getGreen());
+        tv_j_green.setText(Integer.toString(color.getGreen()));
+        sb_j_blue.setProgress(color.getBlue());
+        tv_j_blue.setText(Integer.toString(color.getBlue()));
+        main.setBackgroundColor(rgb(color.getRed(),color.getGreen(),color.getBlue()));
+
+
+    }
+
     private void filllistview()
     {
-        clAdapter = new ColorDataAdapter(this, ListOfColors );
+        clAdapter = new ColorDataAdapter(this, ListOfColors);
         lv_j_ListOfColors.setAdapter(clAdapter);
     }
+    private void onseekbarchangelistner()
+    {
+        sb_j_red.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+               tv_j_red.setText(Integer.toString(progress));
+               BackroundChange();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        sb_j_green.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tv_j_green.setText(Integer.toString(progress));
+                BackroundChange();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        sb_j_blue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tv_j_blue.setText(Integer.toString(progress));
+                BackroundChange();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    private void onButtonClickListner()
+    {
+        btn_j_addcolor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addcolor();
+            }
+        });
+    }
+    private void BackroundChange()
+    {
+        int r = Integer.parseInt(tv_j_red.getText().toString());
+        int g = Integer.parseInt(tv_j_green.getText().toString());
+        int b =Integer.parseInt(tv_j_blue.getText().toString());
+        main.setBackgroundColor(rgb(r,g,b));
+        String h = String.format("#%02x%02x%02x",r,g,b);
+        tv_j_hexa.setText(h);
+
+        if (androidx.core.graphics.ColorUtils.calculateLuminance(rgb(r,g,b)) < 0.5)
+        {
+            // Background is dark, set text to white
+            tv_j_red.setTextColor(Color.WHITE);
+            tv_j_green.setTextColor(Color.WHITE);
+            tv_j_blue.setTextColor(Color.WHITE);
+            tv_j_hexa.setTextColor(Color.WHITE);
+            tv_j_red3.setTextColor(Color.WHITE);
+            tv_j_green2.setTextColor(Color.WHITE);
+            tv_j_blue2.setTextColor(Color.WHITE);
+            tv_j_hexatext.setTextColor(Color.WHITE);
+
+        }
+        else
+        {
+            // Background is light, set text to black
+            tv_j_red.setTextColor(Color.BLACK);
+            tv_j_green.setTextColor(Color.BLACK);
+            tv_j_blue.setTextColor(Color.BLACK);
+            tv_j_hexa.setTextColor(Color.BLACK);
+            tv_j_red3.setTextColor(Color.BLACK);
+            tv_j_green2.setTextColor(Color.BLACK);
+            tv_j_blue2.setTextColor(Color.BLACK);
+            tv_j_hexatext.setTextColor(Color.BLACK);
+
+        }
+
+       // ColorData newcolor = new ColorData(r,g,b,h);
+      //  ListOfColors.add(newcolor);
+      //  filllistview();
+
+    }
+    private void addcolor()
+    {
+        int r = Integer.parseInt(tv_j_red.getText().toString());
+        int g = Integer.parseInt(tv_j_green.getText().toString());
+        int b =Integer.parseInt(tv_j_blue.getText().toString());
+
+        main.setBackgroundColor(rgb(r,g,b));
+
+        String h = String.format("#%02x%02x%02x",r,g,b);
+
+        ColorData newcolor = new ColorData(r,g,b,h);
+        ListOfColors.add(newcolor);
+
+        filllistview();
+    }
+
+
 }
